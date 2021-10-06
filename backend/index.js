e57c005a55af31cbad1f752fb09e987f57ee0e33
Vikas Rajbhar <rajbhar67@gmail.com>
2021-10-06 13:23:32 +0530

    removed unused imports in backend

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,17 @@ const { ApolloServer } = require("apollo-server-express");
 const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
 const express = require("express");
 const http = require("http");
-const { execute, subscribe } = require("graphql");
-const { SubscriptionServer } = require("subscriptions-transport-ws");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const {
   ApolloServerPluginLandingPageGraphQLPlayground,
 } = require("apollo-server-core");
-const { PubSub } = require("graphql-subscriptions");
 const mongoose = require("mongoose");
-const multer = require("multer");
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config");
 
+const PORT = process.env.PORT || 5000;
 async function startApolloServer(typeDefs, resolvers) {
   const app = express();
   const httpServer = http.createServer(app);
@@ -36,9 +33,9 @@ async function startApolloServer(typeDefs, resolvers) {
       {
         async serverWillStart() {
           return {
-            async drainServer() {
-              subscriptionServer.close();
-            },
+            // async drainServer() {
+            //   subscriptionServer.close();
+            // },
           };
         },
       },
@@ -56,28 +53,11 @@ async function startApolloServer(typeDefs, resolvers) {
 
     path: "/",
   });
-  const subscriptionServer = SubscriptionServer.create(
-    {
-      schema,
 
-      execute,
-      subscribe,
-      onConnect(connectionParams, webSocket, context) {
-        console.log("Connected!");
-      },
-      onDisconnect(webSocket, context) {
-        console.log("Disconnected!");
-      },
-    },
-    {
-      server: httpServer,
-
-      path: server.graphqlPath,
-    }
+  await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
   );
-
-  await new Promise((resolve) => httpServer.listen({ port: 5000 }, resolve));
-  console.log(`🚀 Server ready at http://localhost:5000${server.graphqlPath}`);
 }
 
 startApolloServer(typeDefs, resolvers);
